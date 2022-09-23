df293f1dc603c0907347b6bcad7930651f267c1b
MDNajeebUddin <mdnajeebuddin765@gmail.com>
2022-09-23 20:27:50 +0530

    Chat Box

---
+++ Python/Chatbox.py
@@ -0,0 +1,14 @@
+def hello(name= "world!"):
+    print(f"hello,{name},How are you ?")
+    ans=input();
+    if ans == "fine":
+        print("very good!")
+    elif ans == "not fine":
+        print("what is wrong ?")
+        x=input()
+        print("so sorry to hear that")
+
+    else:
+      print("have a grate day!")
+    pass
+
