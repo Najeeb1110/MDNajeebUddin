ab5ec6431bcfeb61e2cdc03bfc88b63f0a0de11b
MDNajeebUddin <mdnajeebuddin765@gmail.com>
2022-09-23 19:52:10 +0530

    Covid Vaccination Project

---
+++ Python/covid.py
@@ -0,0 +1,9 @@
+#Covid vaccination Project 
+name=input("enter your name:")
+age=int(input("enter your age:"))
+
+if age > 16:
+    print(f"{name},your are approved for vaccin!")
+
+else:
+    print(f"sorry{name},your are decline")
