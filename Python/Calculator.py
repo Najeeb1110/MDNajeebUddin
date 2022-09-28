d1ed2e0941f523dd837dffc7b8bd93c4b537fbd3
MDNajeebUddin <mdnajeebuddin765@gmail.com>
2022-09-28 12:27:51 +0530

    Calculator

--- Python/Calculator.py
+++ Python/Calculator.py
@@ -1,3 +1,7 @@
+# Date 27-sep-2022
+# Programmer : MD.Najeeb Uddin
+
+# Function Definations 
 def add(P,Q):
     return P + Q
 def subtract(P,Q):
@@ -12,10 +16,12 @@ print("a.Add")
 print("b.Subtract")
 print("c.Multiply")
 print("d.Divide")
+# Taking Inputs
 Choice = input("Please enter choice(a/b/c/d):")
 
 num_1 = int(input("Please enter the first number :c"))
 num_2 = int(input("Please enter the second number :"))
+# Comparing the Choice 
 if Choice == 'a':
     print(num_1,"+",num_2,"=",add(num_1,num_2))
 elif Choice == 'b':
