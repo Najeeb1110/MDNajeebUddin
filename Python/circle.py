2eb073ef10405a1e17174266f108f5ddb2695c3b
MDNajeebUddin <mdnajeebuddin765@gmail.com>
2022-10-31 13:39:13 +0530

    Creating Circle

---
+++ Python/circle.py
@@ -0,0 +1,20 @@
+#MD Najeeb uddin
+#Creating Circle Using Python
+import turtle
+
+t = turtle.Turtle()
+s = turtle.Screen()
+
+s.colormode(255)
+
+R = 0
+G = 0
+B = 0
+
+s.bgcolor((R, G, B))
+
+t.color('pink')
+
+r = 100
+
+t.circle(r)
